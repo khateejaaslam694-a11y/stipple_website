3fdb2b68dec001c13a7f7eece551072b2febbd96
khadija Ejaz <224268416+khateejaaslam694-a11y@users.noreply.github.com>
2026-03-30 00:33:31 +0500

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -3,6 +3,14 @@
 // ============================================
 
 document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.getElementById('menuToggle');
+const navLinks = document.getElementById('navLinks');
+
+if (menuToggle && navLinks) {
+    menuToggle.addEventListener('click', function () {
+        navLinks.classList.toggle('show');
+    });
+}
     const galleryCountEl = document.getElementById('galleryCount');
     const totalArtworks = document.getElementById('totalArtworks');
     const exportBtn = document.getElementById('exportBtn');
